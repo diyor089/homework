bee2c2b90ce45bc784f5833e65ad40a133dc2112
diyor089 <diyornurmetov52@gmail.com>
2024-08-12 13:30:21 +0500

    add code

---
+++ python_modullari.py
@@ -0,0 +1,59 @@
+# Masala 1: Kvadrat ildizni topish
+# Masala: Sizga x=144 berilgan. math.sqrt funksiyasidan foydalanib, x-ning kvadrat ildizini toping.
+#
+# Masala 2: Darajaga oshirish
+# Masala: Sizga a=2 va b=10 berilgan. math.pow funksiyasidan foydalanib, a ning b-darajasini hisoblang.
+#
+# Masala 3: Sinus qiymatini topish
+# Masala: Sizga burchak a=30 gradus burchak  berilgan. math.sin funksiyasidan foydalanib,a ning sinusini toping. Eslatma: math.sin radianlar bilan ishlaydi, shuning uchun burchakni radianlarga o'girish kerak bo'ladi.
+#
+# Masala 4: Tasodifiy son yaratish
+# Masala: Sizga 𝑎=55 va b=155 berilgan. random.randint funksiyasidan foydalanib, shu oraliqda tasodifiy butun son yarating.
+#
+# Masala 5: Tasodifiy element tanlash
+# Masala: Sizga quyidagi ro'yxat berilgan: colors = ["qizil", "yashil", "ko'k", "sariq", "qora"]. random.choice funksiyasidan foydalanib, ro'yxatdan tasodifiy rangni tanlang.
+
+
+
+
+                                # /4/
+
+# import random as r
+#
+# son = r.randint(55,155)
+# print(son)
+
+
+                               # /1/
+
+
+# import math
+#
+# x=144
+# print(math.sqrt(x))
+
+
+
+                            # /5/
+
+
+# import random
+#
+# colors = ["qizil", "yashil", "ko'k", "sariq", "qora"]
+# tasodifiy_rang = random.choice(colors)
+#
+# print(tasodifiy_rang)
+
+
+                               # /2/
+
+
+# import math
+#
+# a = 2
+# b = 10
+#
+# daraja = math.pow(a, b)
+#
+# print(daraja)
+
